4abd3c681ae87e9de9659e42d9e014646d5c2cf6
Remy Pierre <remy.pierre.dev@outlook.fr>
2023-03-18 22:03:45 +0100

    connect to mongoDB

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const connectDB = require("./DB/connection");
 const auth = require("./routes/auth");
 const books = require("./routes/books");
+require("dotenv").config();
 
 const app = express();
-const port = 5000;
+const port = process.env.port || 5000;
 
 /* middlewares */
 app.use([express.urlencoded({ extended: false }), express.json()]);
@@ -16,7 +18,15 @@ app.get("/", (req, res) => {
   res.send("<h1>Home page</h1>");
 });
 
-/* server listener */
-app.listen(port, () => {
-  console.log(`Serveur is waiting, port: ${port}`);
-});
+/* connect DB then start server or close if catch err */
+const start = (async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Serveur is waiting, port: ${port}`));
+  } catch (err) {
+    console.log(
+      "Erreur lors de la connection à la DB. Fermeture du serveur.",
+      err
+    );
+  }
+})();
